5a376898ffe0dc4a99b08e1c0fb9b9b3839b9eca
Michael Bachmann <mbachmann@bbmsoft.net>
2024-08-03 14:39:59 +0200

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 NAME=$(jq <package.json '.name' -r)
 VERSION=$(jq <package.json '.version' -r)
-docker build -t "babymotte/$NAME:$VERSION" -t "babymotte/$NAME:latest" --push .
+git tag "v$VERSION" || exit 1
+docker build -t "babymotte/$NAME:$VERSION" -t "babymotte/$NAME:latest" --push . || exit 1
 helm upgrade "$NAME" ./chart
-# git push
+git push && git push --tags
