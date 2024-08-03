5b218c5dc1bc614db11f43f446e1dc9cd788b780
Michael Bachmann <mbachmann@bbmsoft.net>
2024-08-03 14:26:56 +0200

    disabled probes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 NAME=$(jq <package.json '.name' -r)
 VERSION=$(jq <package.json '.version' -r)
 docker build -t "babymotte/$NAME:$VERSION" -t "babymotte/$NAME:latest" --push .
-helm uninstall "$NAME"
-helm install "$NAME" ./chart
+helm upgrade "$NAME" ./chart
 # git push
